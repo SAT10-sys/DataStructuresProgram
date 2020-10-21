ca6c593729024b7568626e359abf996856e1aeca
Satantra Tewari <satantra1508@gmail.com>
2020-10-21 22:48:49 +0530

    [satantra] linked list created of three elements 56,30,70

--- Data_Structures_Problem/Program.cs
+++ Data_Structures_Problem/Program.cs
@@ -7,6 +7,12 @@ namespace Data_Structures_Problem
         static void Main(string[] args)
         {
             Console.WriteLine(" Welcome To Data Structures Problem ");
+            LinkedList linkedList = new LinkedList();
+            linkedList.AddNode(56);
+            linkedList.AddNode(30);
+            linkedList.AddNode(70);
+            Console.WriteLine("Linked List is: ");
+            linkedList.Display();
         }
     }
 }
